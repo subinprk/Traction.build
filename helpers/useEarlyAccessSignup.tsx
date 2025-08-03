3abc06195ecdc6776b8abff37bfd4e907970c138
Jonathanamir1 <jonathanamir1@gmail.com>
2025-08-03 15:59:38 +0200

    initialized git

--- helpers/useEarlyAccessSignup.tsx
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { postEarlyAccess, InputType, OutputType } from "../endpoints/early-access_POST.schema";
-
-export const useEarlyAccessSignup = () => {
-  return useMutation<OutputType, Error, InputType>({
-    mutationFn: (data: InputType) => postEarlyAccess(data),
-  });
-};
--- process.env.d.ts
+++
@@ -1,9 +0,0 @@
-type ProcessEnvShouldBeSuppliedByResources = {
-FLOOT_DATABASE_URL: string;
-NODE_ENV: string;
-}
-
-// Override the global process variable
-declare var process: {
-  env: ProcessEnvShouldBeSuppliedByResources;
-};
